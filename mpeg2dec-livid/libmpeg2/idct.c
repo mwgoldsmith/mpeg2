e8721265fde12ea917d30c0f4c5a5d4e1a4dbf26
walken <walken@69194b2d-ca13-0410-9ec0-9203f42c7898>
2000-08-17 04:09:20 +0000

    fixed the c idct routine.
    note that this routine complies with ieee1180 precision requirements,
    while the mmx routine does not. On some stream with lots of consecutive
    P frames, you can actually notice the difference.

--- mpeg2dec-livid/libmpeg2/idct.c
+++ mpeg2dec-livid/libmpeg2/idct.c
@@ -50,10 +50,6 @@
 #include "idct_mlib.h"
 
 
-// Fixed Point
-// to convert from 16.0 to 12.4 fixed point
-#define FP 4
-
 #define W1 2841 /* 2048*sqrt(2)*cos(1*pi/16) */
 #define W2 2676 /* 2048*sqrt(2)*cos(2*pi/16) */
 #define W3 2408 /* 2048*sqrt(2)*cos(3*pi/16) */
@@ -72,20 +68,9 @@ static void idct_col_s16_c(sint_16 *blk);
 static void idct_block_c(sint_16 *block);
 static void idct_end_c(void);
 
-// Clamp to [-256,255]
-static sint_16 clip_tbl[1024]; /* clipping table */
-static sint_16 *clip;
-
 void 
 idct_init(void)
 {
-	sint_32 i;
-
-	clip = clip_tbl + 512;
-
-	for (i= -512; i< 512; i++)
-		clip[i] = (i < -256) ? -256 : ((i > 255) ? 255 : i);
-
 #ifdef __i386__
 	if(config.flags & MPEG2_MMX_ENABLE)
 	{
@@ -124,61 +109,61 @@ idct_init(void)
 static void inline
 idct_row_c(sint_16 *blk)
 {
-  sint_32 x0, x1, x2, x3, x4, x5, x6, x7, x8;
+	int x0, x1, x2, x3, x4, x5, x6, x7, x8;
 
-  x1 = blk[4]<<11;
-  x2 = blk[6];
-  x3 = blk[2];
-  x4 = blk[1];
-  x5 = blk[7];
-  x6 = blk[5];
-  x7 = blk[3];
+	x1 = blk[4]<<11;
+	x2 = blk[6];
+	x3 = blk[2];
+	x4 = blk[1];
+	x5 = blk[7];
+	x6 = blk[5];
+	x7 = blk[3];
 
-  /* shortcut */
-  if (!(x1 | x2 | x3 | x4 | x5 | x6 | x7 ))
-  {
-    blk[0]=blk[1]=blk[2]=blk[3]=blk[4]=blk[5]=blk[6]=blk[7]=blk[0]<<3;
-    return;
-  }
+	/* shortcut */
+	if (!(x1 | x2 | x3 | x4 | x5 | x6 | x7 ))
+	{
+		blk[0]=blk[1]=blk[2]=blk[3]=blk[4]=blk[5]=blk[6]=blk[7]=blk[0]<<3;
+		return;
+	}
 
-  x0 = (blk[0]<<11) + 128; /* for proper rounding in the fourth stage */
+	x0 = (blk[0]<<11) + 128; /* for proper rounding in the fourth stage */
 
-  /* first stage */
-  x8 = W7*(x4+x5);
-  x4 = x8 + (W1-W7)*x4;
-  x5 = x8 - (W1+W7)*x5;
-  x8 = W3*(x6+x7);
-  x6 = x8 - (W3-W5)*x6;
-  x7 = x8 - (W3+W5)*x7;
+	/* first stage */
+	x8 = W7*(x4+x5);
+	x4 = x8 + (W1-W7)*x4;
+	x5 = x8 - (W1+W7)*x5;
+	x8 = W3*(x6+x7);
+	x6 = x8 - (W3-W5)*x6;
+	x7 = x8 - (W3+W5)*x7;
   
-  /* second stage */
-  x8 = x0 + x1;
-  x0 -= x1;
-  x1 = W6*(x3+x2);
-  x2 = x1 - (W2+W6)*x2;
-  x3 = x1 + (W2-W6)*x3;
-  x1 = x4 + x6;
-  x4 -= x6;
-  x6 = x5 + x7;
-  x5 -= x7;
+	/* second stage */
+	x8 = x0 + x1;
+	x0 -= x1;
+	x1 = W6*(x3+x2);
+	x2 = x1 - (W2+W6)*x2;
+	x3 = x1 + (W2-W6)*x3;
+	x1 = x4 + x6;
+	x4 -= x6;
+	x6 = x5 + x7;
+	x5 -= x7;
   
-  /* third stage */
-  x7 = x8 + x3;
-  x8 -= x3;
-  x3 = x0 + x2;
-  x0 -= x2;
-  x2 = (181*(x4+x5)+128)>>8;
-  x4 = (181*(x4-x5)+128)>>8;
+	/* third stage */
+	x7 = x8 + x3;
+	x8 -= x3;
+	x3 = x0 + x2;
+	x0 -= x2;
+	x2 = (181*(x4+x5)+128)>>8;
+	x4 = (181*(x4-x5)+128)>>8;
   
-  /* fourth stage */
-  blk[0] = (x7+x1)>>8;
-  blk[1] = (x3+x2)>>8;
-  blk[2] = (x0+x4)>>8;
-  blk[3] = (x8+x6)>>8;
-  blk[4] = (x8-x6)>>8;
-  blk[5] = (x0-x4)>>8;
-  blk[6] = (x3-x2)>>8;
-  blk[7] = (x7-x1)>>8;
+	/* fourth stage */
+	blk[0] = (x7+x1)>>8;
+	blk[1] = (x3+x2)>>8;
+	blk[2] = (x0+x4)>>8;
+	blk[3] = (x8+x6)>>8;
+	blk[4] = (x8-x6)>>8;
+	blk[5] = (x0-x4)>>8;
+	blk[6] = (x3-x2)>>8;
+	blk[7] = (x7-x1)>>8;
 }
 
 
@@ -195,68 +180,67 @@ idct_row_c(sint_16 *blk)
 static void inline
 idct_col_s16_c(sint_16 *blk)
 {
-  sint_32 x0, x1, x2, x3, x4, x5, x6, x7, x8;
+	int x0, x1, x2, x3, x4, x5, x6, x7, x8;
 
-  /* shortcut */
-  x1 = blk[8*4]<<8; 
-  x2 = blk[8*6]; 
-  x3 = blk[8*2];
-  x4 = blk[8*1];
-  x5 = blk[8*7]; 
-  x6 = blk[8*5];
-  x7 = blk[8*3];
+	/* shortcut */
+	x1 = blk[8*4]<<8;
+	x2 = blk[8*6];
+	x3 = blk[8*2];
+	x4 = blk[8*1];
+	x5 = blk[8*7];
+	x6 = blk[8*5];
+	x7 = blk[8*3];
 
-  if (!(x1  | x2 | x3 | x4 | x5 | x6 | x7 ))
-  {
-    blk[8*0]=blk[8*1]=blk[8*2]=blk[8*3]=blk[8*4]=blk[8*5]=blk[8*6]=blk[8*7]=
-      clip[(blk[8*0]+32)>>6];
-    return;
-  }
+	if (!(x1  | x2 | x3 | x4 | x5 | x6 | x7 ))
+	{
+		blk[8*0]=blk[8*1]=blk[8*2]=blk[8*3]=blk[8*4]=blk[8*5]=blk[8*6]=blk[8*7]=(blk[8*0]+32)>>6;
+		return;
+	}
 
-  x0 = (blk[8*0]<<8) + 8192;
+	x0 = (blk[8*0]<<8) + 8192;
 
-  /* first stage */
-  x8 = W7*(x4+x5) + 4;
-  x4 = (x8+(W1-W7)*x4)>>3;
-  x5 = (x8-(W1+W7)*x5)>>3;
-  x8 = W3*(x6+x7) + 4;
-  x6 = (x8-(W3-W5)*x6)>>3;
-  x7 = (x8-(W3+W5)*x7)>>3;
+	/* first stage */
+	x8 = W7*(x4+x5) + 4;
+	x4 = (x8+(W1-W7)*x4)>>3;
+	x5 = (x8-(W1+W7)*x5)>>3;
+	x8 = W3*(x6+x7) + 4;
+	x6 = (x8-(W3-W5)*x6)>>3;
+	x7 = (x8-(W3+W5)*x7)>>3;
   
-  /* second stage */
-  x8 = x0 + x1;
-  x0 -= x1;
-  x1 = W6*(x3+x2) + 4;
-  x2 = (x1-(W2+W6)*x2)>>3;
-  x3 = (x1+(W2-W6)*x3)>>3;
-  x1 = x4 + x6;
-  x4 -= x6;
-  x6 = x5 + x7;
-  x5 -= x7;
+	/* second stage */
+	x8 = x0 + x1;
+	x0 -= x1;
+	x1 = W6*(x3+x2) + 4;
+	x2 = (x1-(W2+W6)*x2)>>3;
+	x3 = (x1+(W2-W6)*x3)>>3;
+	x1 = x4 + x6;
+	x4 -= x6;
+	x6 = x5 + x7;
+	x5 -= x7;
   
-  /* third stage */
-  x7 = x8 + x3;
-  x8 -= x3;
-  x3 = x0 + x2;
-  x0 -= x2;
-  x2 = (181*(x4+x5)+128)>>8;
-  x4 = (181*(x4-x5)+128)>>8;
+	/* third stage */
+	x7 = x8 + x3;
+	x8 -= x3;
+	x3 = x0 + x2;
+	x0 -= x2;
+	x2 = (181*(x4+x5)+128)>>8;
+	x4 = (181*(x4-x5)+128)>>8;
   
-  /* fourth stage */
-  blk[8*0] = clip[(x7+x1)>>14];
-  blk[8*1] = clip[(x3+x2)>>14];
-  blk[8*2] = clip[(x0+x4)>>14];
-  blk[8*3] = clip[(x8+x6)>>14];
-  blk[8*4] = clip[(x8-x6)>>14];
-  blk[8*5] = clip[(x0-x4)>>14];
-  blk[8*6] = clip[(x3-x2)>>14];
-  blk[8*7] = clip[(x7-x1)>>14];
+	/* fourth stage */
+	blk[8*0] = (x7+x1)>>14;
+	blk[8*1] = (x3+x2)>>14;
+	blk[8*2] = (x0+x4)>>14;
+	blk[8*3] = (x8+x6)>>14;
+	blk[8*4] = (x8-x6)>>14;
+	blk[8*5] = (x0-x4)>>14;
+	blk[8*6] = (x3-x2)>>14;
+	blk[8*7] = (x7-x1)>>14;
 }
 
 void
 idct_block_c(sint_16 *block)
 {
-	uint_32 i;
+	int i;
 
 	for (i=0; i<8; i++)
 		idct_row_c(block + 8*i);
