a8ca29a60c58fc801e28677e1b02ee41936e2662
dent <dent@69194b2d-ca13-0410-9ec0-9203f42c7898>
2000-09-01 19:07:46 +0000

    added dirty conditionals (__OMS__), for beeing able to move the decoder back into the oms_devel tree.
    ++dent

--- mpeg2dec-livid/libmpeg2/decode.c
+++ mpeg2dec-livid/libmpeg2/decode.c
@@ -22,13 +22,20 @@
  * the Free Software Foundation, 
  *
  */
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
 
+#ifdef __OMS__
+#include <oms/oms.h>
+#include <oms/plugin/codec_video.h>
+
+#define vo_functions_t	plugin_codec_video_t
+#endif
+ 
 #include "config.h"
 #include "mpeg2.h"
 #include "mpeg2_internal.h"
@@ -49,9 +56,6 @@ picture_t picture;
 //global config struct
 mpeg2_config_t config;
 
-//pointers to the display interface functions
-vo_functions_t video_out;
-
 //
 //the current start code chunk we are working on
 //we max out at 65536 bytes as that is the largest
@@ -63,11 +67,9 @@ static uint32_t shift = 0;
 static uint32_t is_display_initialized = 0;
 static uint32_t is_sequence_needed = 1;
 
-void mpeg2_init (vo_functions_t * foo)
-{
-    //copy the display interface function pointers
-    video_out = *foo;
 
+void mpeg2_init (void)
+{
     //FIXME setup config properly
     config.flags = MPEG2_MMX_ENABLE | MPEG2_MLIB_ENABLE;
     //config.flags = 0;
@@ -81,7 +83,8 @@ void mpeg2_init (vo_functions_t * foo)
     motion_comp_init ();
 }
 
-static void decode_allocate_image_buffers (picture_t * picture)
+
+static void decode_allocate_image_buffers (vo_functions_t *video_out, picture_t * picture)
 {
     int frame_size;
     int slice_size;
@@ -91,21 +94,22 @@ static void decode_allocate_image_buffers (picture_t * picture)
     slice_size = picture->coded_picture_width * 16;
 
     //FIXME the next step is to give a vo_image_buffer_t* to dislay_slice (or eqiv)
-    tmp = video_out.allocate_image_buffer (16,picture->coded_picture_width,0);
+    tmp = video_out->allocate_image_buffer (16,picture->coded_picture_width,0);
     picture->throwaway_frame[0] = tmp->base;
     picture->throwaway_frame[1] = picture->throwaway_frame[0] + slice_size;
     picture->throwaway_frame[2] = picture->throwaway_frame[1] + slice_size/4;
 
-    tmp = video_out.allocate_image_buffer (picture->coded_picture_height,picture->coded_picture_width,0);
+    tmp = video_out->allocate_image_buffer (picture->coded_picture_height,picture->coded_picture_width,0);
     picture->backward_reference_frame[0] = tmp->base;
     picture->backward_reference_frame[1] = picture->backward_reference_frame[0] + frame_size;
     picture->backward_reference_frame[2] = picture->backward_reference_frame[1] + frame_size/4;
 
-    tmp = video_out.allocate_image_buffer (picture->coded_picture_height,picture->coded_picture_width,0);
+    tmp = video_out->allocate_image_buffer (picture->coded_picture_height,picture->coded_picture_width,0);
     picture->forward_reference_frame[0] = tmp->base;
     picture->forward_reference_frame[1] = picture->forward_reference_frame[0] + frame_size;
     picture->forward_reference_frame[2] = picture->forward_reference_frame[1] + frame_size/4;
 }
+
 
 static void decode_reorder_frames (void)
 {
@@ -133,7 +137,8 @@ static void decode_reorder_frames (void)
     }
 }
 
-static int parse_chunk (int code, uint8_t * buffer)
+
+static int parse_chunk (vo_functions_t *video_out, int code, uint8_t *buffer)
 {
     int is_frame_done = 0;
 
@@ -153,8 +158,8 @@ static int parse_chunk (int code, uint8_t * buffer)
 	is_sequence_needed = 0;
 
 	if (!is_display_initialized) {
-	    video_out.init (picture.coded_picture_width,picture.coded_picture_height,0,0);
-	    decode_allocate_image_buffers (&picture);
+	    video_out->init (picture.coded_picture_width,picture.coded_picture_height,0,0);
+	    decode_allocate_image_buffers (video_out, &picture);
 	    is_display_initialized = 1;
 	}
 	break;
@@ -168,7 +173,7 @@ static int parse_chunk (int code, uint8_t * buffer)
 	    is_frame_done = slice_process (&picture, code, buffer);
 
 	    if (picture.picture_coding_type == B_TYPE) {
-		video_out.draw_slice (picture.throwaway_frame, code - 1);
+		video_out->draw_slice (picture.throwaway_frame, code - 1);
 
 		picture.current_frame[0] = picture.throwaway_frame[0] -
 		    code * 16 * picture.coded_picture_width;
@@ -186,19 +191,20 @@ static int parse_chunk (int code, uint8_t * buffer)
 		foo[2] = picture.forward_reference_frame[2] + (code-1) * 8 *
 		    picture.coded_picture_width/2;
 
-		video_out.draw_slice (foo, code-1);
-		//video_out.draw_frame (picture.forward_reference_frame);
+		video_out->draw_slice (foo, code-1);
+		//video_out->draw_frame (picture.forward_reference_frame);
 	    }
 
 	    if (is_frame_done)
-		video_out.flip_page ();
+		video_out->flip_page ();
 	}
     }
 
     return is_frame_done;
 }
 
-int mpeg2_decode_data (uint8_t * current, uint8_t * end) 
+
+int mpeg2_decode_data (vo_functions_t *video_out, uint8_t * current, uint8_t * end) 
 {
     static uint8_t code = 0xff;
     //static uint8_t chunk_buffer[65536];
@@ -222,7 +228,7 @@ int mpeg2_decode_data (uint8_t * current, uint8_t * end)
 
 	/* found start_code following chunk */
 
-	ret += parse_chunk (code, chunk_buffer);
+	ret += parse_chunk (video_out, code, chunk_buffer);
 
 	/* done with header or slice, prepare for next one */
 
