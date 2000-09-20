7dade399fe4263755f5feab9ac704b618323b4b4
dent <dent@69194b2d-ca13-0410-9ec0-9203f42c7898>
2000-09-20 13:48:28 +0000

    added OMS specific portions. (dec_mpeg2.c is the oms plugin)
    	++dent

---
+++ mpeg2dec-livid/libmpeg2/dec_mpeg2.c
@@ -0,0 +1,90 @@
+//PLUGIN_INFO(INFO_NAME, "MPEG2 video decoder");
+//PLUGIN_INFO(INFO_AUTHOR, "Aaron Holtzman <aholtzma@ess.engr.uvic.ca>");
+//PLUGIN_INFO(INFO_AUTHOR, "Thomas Mirlacher <dent@linuxvideo.org>");
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#include <oms/oms.h>
+#include <oms/plugin/codec_video.h>
+
+#include "config.h"
+#include "mpeg2.h"
+
+static int _mpeg2dec_open	(plugin_t *plugin, void *foo);
+static int _mpeg2dec_close	(plugin_t *plugin);
+static int _mpeg2dec_read	(plugin_codec_video_t *plugin, buf_t *buf, buf_entry_t *buf_entry);
+
+static plugin_codec_video_t codec_mpeg2dec = {
+        open:		_mpeg2dec_open,
+        close:		_mpeg2dec_close,
+        read:		_mpeg2dec_read,
+};
+
+
+/************************************************/
+
+
+/**
+ *
+ **/
+
+static int _mpeg2dec_open (plugin_t *plugin, void *foo)
+{
+	mpeg2_init ();
+	return 0;
+}
+
+
+/**
+ *
+ **/
+
+static int _mpeg2dec_close (plugin_t *plugin)
+{
+	return 0;
+}
+
+
+/**
+ *
+ **/
+
+static int _mpeg2dec_read (plugin_codec_video_t *plugin, buf_t *buf, buf_entry_t *buf_entry)
+{
+	mpeg2_decode_data (plugin->output, buf_entry->data, buf_entry->data+buf_entry->data_len);
+
+	return 0;
+}
+
+/*****************************************/
+
+
+/**
+ * Initialize Plugin.
+ **/
+
+
+int plugin_init (char *whoami)
+{
+	pluginRegister (whoami,
+		PLUGIN_ID_CODEC_VIDEO,
+		"mpg2",
+		NULL,
+		NULL,
+		&codec_mpeg2dec);
+
+	return 0;
+}
+
+
+/**
+ * Cleanup Plugin.
+ **/
+
+void plugin_exit (void)
+{
+}
