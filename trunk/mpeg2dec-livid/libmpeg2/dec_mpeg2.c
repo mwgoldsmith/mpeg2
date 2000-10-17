b83ec7999a055a99d6ccf92538c3d1472c5f9cc4
bruno <bruno@69194b2d-ca13-0410-9ec0-9203f42c7898>
2000-10-17 06:59:35 +0000

    pasted fps code from mpeg2dec, looks pretty nice now...
    - bruno

--- trunk/mpeg2dec-livid/libmpeg2/dec_mpeg2.c
+++ trunk/mpeg2dec-livid/libmpeg2/dec_mpeg2.c
@@ -26,53 +26,83 @@ static plugin_codec_video_t codec_mpeg2dec = {
 	set_flag:	_mpeg2dec_set_flag,
 };
 
-
+static struct timeval tv_beg, tv_end, tv_start;
+static uint32_t elapsed;
+static uint32_t total_elapsed;
+static uint32_t last_count = 0;
+static uint32_t frame_counter = 0;
 /************************************************/
 
+static void print_fps (int final) 
+{
+    int fps, tfps, frames;
+	
+    gettimeofday (&tv_end, NULL);
 
+    if (frame_counter++ == 0) {
+	tv_start = tv_beg = tv_end;
+    }
+
+    elapsed = (tv_end.tv_sec - tv_beg.tv_sec) * 100 +
+	(tv_end.tv_usec - tv_beg.tv_usec) / 10000;
+    total_elapsed = (tv_end.tv_sec - tv_start.tv_sec) * 100 +
+	(tv_end.tv_usec - tv_start.tv_usec) / 10000;
+
+    if (final) {
+	if (total_elapsed) 
+	    tfps = frame_counter * 10000 / total_elapsed;
+	else
+	    tfps = 0;
+
+	fprintf (stderr,"\n%d frames decoded in %d.%02d "
+		 "seconds (%d.%02d fps)\n", frame_counter,
+		 total_elapsed / 100, total_elapsed % 100,
+		 tfps / 100, tfps % 100);
+
+	return;
+    }
+
+    if (elapsed < 50)	/* only display every 0.50 seconds */
+	return;
+
+    tv_beg = tv_end;
+    frames = frame_counter - last_count;
+
+    fps = frames * 10000 / elapsed;			/* 100x */
+    tfps = frame_counter * 10000 / total_elapsed;	/* 100x */
+
+    fprintf (stderr, "%d frames in %d.%02d sec (%d.%02d fps), "
+	     "%d last %d.%02d sec (%d.%02d fps)\033[K\r", frame_counter,
+	     total_elapsed / 100, total_elapsed % 100,
+	     tfps / 100, tfps % 100, frames, elapsed / 100, elapsed % 100,
+	     fps / 100, fps % 100);
+
+    last_count = frame_counter;
+}
+ 
 static int _mpeg2dec_open (plugin_t *plugin, void *foo)
 {
 	mpeg2_init ();
 	return 0;
 }
 
-
 static int _mpeg2dec_close (plugin_t *plugin)
 {
+	//FIXME: how are we supposed to call mpeg2_close without
+	//the output struct?
+	//mpeg2_close(plugin->output);
+	print_fps(1);	
 	return 0;
-}
-
-static void compute_stat(void) 
-{
-        static int fps = 0;
-        static struct timeval s, e = { 0, 0 };
-        double time;
-        
-        fps++;
-
-        time = (s.tv_sec + (double) (s.tv_usec * 1e-6)) -
-                (e.tv_sec  + (double) (e.tv_usec * 1e-6));
-
-        gettimeofday(&s, NULL);
-        if ( e.tv_sec == 0 ) 
-                gettimeofday(&e, NULL);
-
-        time = (s.tv_sec + (double) (s.tv_usec * 1e-6)) -
-                (e.tv_sec  + (double) (e.tv_usec * 1e-6));
-
-        if ( time >= 1 ) {
-                printf("%d fps in %fs\n", fps, time);
-                fps = 0;
-                e.tv_sec = s.tv_sec;
-                e.tv_usec = s.tv_usec;
-        }
 }
 
 static int _mpeg2dec_read (plugin_codec_video_t *plugin, buf_t *buf, buf_entry_t *buf_entry)
 {       
-	mpeg2_decode_data (plugin->output, buf_entry->data, buf_entry->data+buf_entry->data_len);
+	int foo;
+	
+	foo = mpeg2_decode_data (plugin->output, buf_entry->data, buf_entry->data+buf_entry->data_len);
 
-        compute_stat();
+        while (foo--)
+		print_fps(0);
 
         return 0;
 }
