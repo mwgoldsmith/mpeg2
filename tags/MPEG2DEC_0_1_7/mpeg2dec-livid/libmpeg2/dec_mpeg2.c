7635d8dbbc8f400ac74398118715550c15050d5e
nobody <nobody@69194b2d-ca13-0410-9ec0-9203f42c7898>
2000-11-03 11:32:33 +0000

    This commit was manufactured by cvs2svn to create tag 'MPEG2DEC_0_1_7'.

---
+++ tags/MPEG2DEC_0_1_7/mpeg2dec-livid/libmpeg2/dec_mpeg2.c
@@ -0,0 +1,92 @@
+//PLUGIN_INFO(INFO_NAME, "MPEG2 video decoder");
+//PLUGIN_INFO(INFO_AUTHOR, "Aaron Holtzman <aholtzma@ess.engr.uvic.ca>");
+//PLUGIN_INFO(INFO_AUTHOR, "Thomas Mirlacher <dent@linuxvideo.org>");
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#include <oms/oms.h>
+#include <oms/plugin/codec_video.h>
+
+#include "config.h"
+#include "mpeg2.h"
+
+static int _mpeg2dec_open	(plugin_t *plugin, void *foo);
+static int _mpeg2dec_close	(plugin_t *plugin);
+static int _mpeg2dec_read	(plugin_t *plugin, buf_t *buf, buf_entry_t *buf_entry);
+static int _mpeg2dec_set_flag	(plugin_codec_video_t *plugin, uint flag, uint val);
+
+static plugin_codec_video_t codec_mpeg2dec = {
+        open:		_mpeg2dec_open,
+        close:		_mpeg2dec_close,
+        read:		_mpeg2dec_read,
+	set_flag:	_mpeg2dec_set_flag,
+};
+
+
+/************************************************/
+
+static int _mpeg2dec_open (plugin_t *plugin, void *foo)
+{
+	mpeg2_init ();
+	return 0;
+}
+
+
+static int _mpeg2dec_close (plugin_t *plugin)
+{
+	//FIXME: how are we supposed to call mpeg2_close without
+	//the output struct?
+	//mpeg2_close(plugin->output);
+	return 0;
+}
+
+
+static int _mpeg2dec_read (plugin_t *plugin, buf_t *buf, buf_entry_t *buf_entry)
+{       
+	mpeg2_decode_data (((plugin_codec_video_t *) plugin)->output, buf_entry->data, buf_entry->data+buf_entry->data_len);
+
+        return 0;
+}
+
+
+static int _mpeg2dec_set_flag (plugin_codec_video_t *plugin, uint flag, uint val)
+{
+	switch (flag) {
+	case FLAG_VIDEO_INITIALIZED:
+		mpeg2_output_init (val);
+		break;
+	case FLAG_VIDEO_DROP_FRAME:
+//                fprintf (stderr, "%c", val ? '-':'+');
+                mpeg2_drop (val);
+		break;
+	default:
+		return -1;
+	}
+
+	return 0;
+}
+
+
+/*****************************************/
+
+
+int plugin_init (char *whoami)
+{
+	pluginRegister (whoami,
+		PLUGIN_ID_CODEC_VIDEO,
+		"mpg1;mpg2",
+		NULL,
+		NULL,
+		&codec_mpeg2dec);
+
+	return 0;
+}
+
+
+void plugin_exit (void)
+{
+}
