766345a9bd818ca73fbbe7b61a8a7296b320e8d5
dent <dent@69194b2d-ca13-0410-9ec0-9203f42c7898>
2000-11-15 18:49:22 +0000

    added new file mm_accel.h edited all OMS_* defines and includes pointing to oms_whatever.
    -> oms and mpeg2dec are now using the same MultiMedia ACCELerator functions
    and defines.

--- mpeg2dec-livid/include/mm_accel.h
+++ mpeg2dec-livid/include/mm_accel.h
@@ -20,11 +20,11 @@
  */
 
 // generic accelerations
-#define OMS_ACCEL_MLIB		0x00000001
+#define MM_ACCEL_MLIB		0x00000001
 
 // x86 accelerations
-#define OMS_ACCEL_X86_MMX	0x80000000
-#define OMS_ACCEL_X86_3DNOW	0x40000000
-#define OMS_ACCEL_X86_MMXEXT	0x20000000
+#define MM_ACCEL_X86_MMX	0x80000000
+#define MM_ACCEL_X86_3DNOW	0x40000000
+#define MM_ACCEL_X86_MMXEXT	0x20000000
 
-uint32_t oms_cpu_accel (void);
+uint32_t mm_accel (void);
