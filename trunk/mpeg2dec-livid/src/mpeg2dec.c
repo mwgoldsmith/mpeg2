34c53610a46adfb80857db0dbbc55e2cc1820fb4
martin <martin@69194b2d-ca13-0410-9ec0-9203f42c7898>
2000-11-23 23:34:24 +0000

    * adjustements for ../video and new reentrant mpeg2dec interface * we build a standalone libvo from the ../video dir

--- trunk/mpeg2dec-livid/src/mpeg2dec.c
+++ trunk/mpeg2dec-livid/src/mpeg2dec.c
@@ -29,7 +29,7 @@
 #include <errno.h>
 #include <inttypes.h>
 
-#include "video_out.h"
+#include "../video/output_video.h"
 #include "mpeg2.h"
 
 #define BUFFER_SIZE 262144
@@ -42,7 +42,7 @@ static uint32_t elapsed;
 static uint32_t total_elapsed;
 static uint32_t last_count = 0;
 static uint32_t demux_dvd = 0;
-static vo_functions_t *video_out;
+static mpeg2dec_t mpeg2dec;
 
 static void print_fps (int final) 
 {
@@ -109,7 +109,7 @@ static void print_usage (char * argv[])
     while (video_out_drivers[i] != NULL) {
 	const vo_info_t *info;
 		
-	info = video_out_drivers[i++]->get_info ();
+	info = video_out_drivers[i++]->vo_info;
 
 	fprintf (stderr, "\t\t\t%s\t%s\n", info->short_name, info->name);
     }
@@ -121,15 +121,17 @@ static void handle_args (int argc, char * argv[])
 {
     int c;
     int i;
+    vo_output_video_t* video_out=NULL;
+    const vo_info_t *info=NULL;
 
     while ((c = getopt (argc,argv,"so:")) != -1) {
 	switch (c) {
 	case 'o':
 	    for (i=0; video_out_drivers[i] != NULL; i++) {
-		const vo_info_t *info = video_out_drivers[i]->get_info ();
-
-		if (strcmp (info->short_name,optarg) == 0)
+	        info = video_out_drivers[i]->vo_info;
+		if (strcmp (info->short_name,optarg) == 0) {
 		    video_out = video_out_drivers[i];
+		}
 	    }
 	    if (video_out == NULL)
 		{
@@ -160,6 +162,11 @@ static void handle_args (int argc, char * argv[])
 	}
     } else
 	in_file = stdin;
+    //
+    // Now we have the output plugin, we need to initialize it
+    // 
+    video_out->open(&video_out,"What Name?");
+    mpeg2_init_ng(&mpeg2dec,video_out,NULL);
 }
 
 static void ps_loop (void)
@@ -225,7 +232,7 @@ static void ps_loop (void)
 		    tmp1 += mpeg1_skip_table [*tmp1 >> 4];
 		}
 		if (tmp1 < tmp2) {
-		    num_frames = mpeg2_decode_data (video_out, tmp1, tmp2);
+		    num_frames = mpeg2_decode_data_ng (&mpeg2dec, tmp1, tmp2);
 		    while (num_frames--)
 			print_fps (0);
 		}
@@ -264,7 +271,7 @@ static void es_loop (void)
 	end = buffer + fread (buffer, 1, BUFFER_SIZE, in_file);
 
 	num_frames =
-	    mpeg2_decode_data (video_out, buffer, end);
+	    mpeg2_decode_data_ng (&mpeg2dec, buffer, end);
 
 	while (num_frames--)
 	    print_fps (0);
@@ -279,8 +286,7 @@ int main (int argc,char *argv[])
     handle_args (argc,argv);
 
     signal (SIGINT, signal_handler);
-
-    mpeg2_init ();
+    
 
     gettimeofday (&tv_beg, NULL);
 
@@ -289,7 +295,7 @@ int main (int argc,char *argv[])
     else
 	es_loop ();
 
-    mpeg2_close (video_out);
+    mpeg2_close_ng (&mpeg2dec);
     print_fps (1);
     return 0;
 }
