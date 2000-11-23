49dee5e4ec3fc64c5872099c22f76c176a48a700
martin <martin@69194b2d-ca13-0410-9ec0-9203f42c7898>
2000-11-23 22:47:45 +0000

    * This makes mpeg2dec reentrant. Note: I hope I have keep all the commits in sync with my tree,       please check it Michel.       It will build soon

--- mpeg2dec-livid/libmpeg2/decode.c
+++ mpeg2dec-livid/libmpeg2/decode.c
@@ -25,132 +25,146 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
-#ifdef __OMS__
-#include <oms/oms.h>
-#include <oms/plugin/output_video.h>
-#endif
 
-#ifndef __OMS__
-#include "video_out.h"
-#endif
-
-#include "mpeg2.h"
 #include "mpeg2_internal.h"
+
+#define __MPEG2_INTERNAL
+#include "mpeg2.h"
+
 #include "mm_accel.h"
 #include "attributes.h"
 #include "mmx.h"
 
 //
-// This (temporary?) hack makes the new libvo interface work
-// with the non-reentrant OMS interface
-// 1. If we compile for OMS we do not add the user_data pointer
-// 2. we set the initstructure types
-#ifdef __OMS__
-#define USERDATA
-#define INITTYPE    plugin_output_video_t
-#define ATTR_TYPE   plugin_output_video_attr_t 
-#else
-static void* user_data=NULL;
-
-#define USERDATA    ,(user_data)
-#define INITTYPE    vo_functions_t
-#define ATTR_TYPE   vo_output_video_attr_t
-#endif
-
-//this is where we keep the state of the decoder
-picture_t picture;
-
-//global config struct
+// here is our global var
+//
 mpeg2_config_t config;
 
-// the maximum chunk size is determined by vbv_buffer_size which is 224K for
-// MP@ML streams. (we make no pretenses ofdecoding anything more than that)
-static uint8_t chunk_buffer[224 * 1024 + 4];
-static uint32_t shift = 0;
 
-static int is_display_initialized = 0;
-static int is_sequence_needed = 1;
-static int drop_flag = 0;
-static int drop_frame = 0;
-static int in_slice = 0;
+//
+// Implementation of old interface [START]
+//
 
-void mpeg2_init (void)
+static mpeg2dec_t mpeg2dec;
+
+void mpeg2_init (void) {
+  mpeg2_init_ng(&mpeg2dec,NULL,NULL);
+}
+
+
+int mpeg2_decode_data (vo_output_video_t *output, 
+		       uint8_t * data_start, uint8_t * data_end) {
+  // we need to do the copy here
+  mpeg2dec.output=output;
+  return mpeg2_decode_data_ng(&mpeg2dec,data_start,data_end);
+}
+  
+void mpeg2_close (vo_output_video_t *output) {
+  mpeg2dec.output=output;
+  mpeg2_close_ng(&mpeg2dec);
+}
+
+void mpeg2_drop (int flag) {
+  mpeg2_drop_ng(&mpeg2dec,flag);
+}
+
+
+void mpeg2_output_init (int flag) {
+  mpeg2_output_init_ng(&mpeg2dec,flag);
+}
+
+
+
+
+//
+// Implementation of old interface [END]
+//
+
+
+//
+// Implementation of new Interface [START]
+//
+
+
+void mpeg2_init_ng (mpeg2dec_t* mpeg2dec,vo_output_video_t* output,
+		    void* user_data) 
 {
-    config.flags = mm_accel () | MM_ACCEL_MLIB;
-	/*try this if you have problems: replace mm_accel () with one of
-	MM_ACCEL_MLIB
-	MM_ACCEL_X86_MMX
-	MM_ACCEL_X86_3DNOW
-	MM_ACCEL_X86_MMXEXT
-	this is only temporary...
-	*/
-
+    
     //intialize the decoder state 
-    shift = 0;
-    is_sequence_needed = 1;
+    mpeg2dec->shift = 0;
+    mpeg2dec->is_display_initialized = 0;
+    mpeg2dec->is_sequence_needed=1;
+    mpeg2dec->drop_flag=0;
+    mpeg2dec->drop_frame=0;
+    mpeg2dec->in_slice=0;
 
-    header_state_init (&picture);
+    // copy output structure into local mpeg2dec structure
+    if (output != NULL) {
+      mpeg2dec->output=(vo_output_video_t *) 
+	malloc (sizeof (vo_output_video_t));
+      memcpy(mpeg2dec->output,output,sizeof (vo_output_video_t));
+    }
+    
+    // opaque user pointer (is passed to the output)
+    mpeg2dec->user_data=user_data;
+    
+    mpeg2dec->chunk_buffer=(uint8_t *) malloc(224 * 1024 + 4);
+    mpeg2dec->chunk_ptr=mpeg2dec->chunk_buffer;
+    mpeg2dec->code=0xff;
+
+    // hm
+    mpeg2dec->picture= (picture_t*) malloc(sizeof(picture_t));
+    memset(mpeg2dec->picture,0,sizeof(picture_t));
+
+    config.flags = mm_accel () | MM_ACCEL_MLIB;
+
+    // initialize supstructures
+    header_state_init (mpeg2dec->picture);
     idct_init ();
     motion_comp_init ();
 }
 
-static void decode_allocate_image_buffers (INITTYPE * output, 
-					   picture_t * picture)
+static void decode_allocate_image_buffers (mpeg2dec_t * mpeg2dec)
 {
+#define picture (mpeg2dec->picture)
+#define output  (mpeg2dec->output)
+#define frame (mpeg2dec->frame)
     int frame_size;
-#ifdef __OMS__
-    img_buf_t *tmp = NULL;
-#else
-    vo_image_buffer_t *tmp = NULL;
-#endif
+
 
     frame_size = picture->coded_picture_width * picture->coded_picture_height;
 
-#ifdef __OMS__
     // allocate images in YV12 format
-    tmp = output->allocate_image_buffer (picture->coded_picture_height, 
-					  picture->coded_picture_width, 
-					  0x32315659);
-#else
-    // allocate images in YV12 format
-    tmp = output->allocate_image_buffer2 (picture->coded_picture_width, 
-					  picture->coded_picture_height, 
-					  0x32315659 USERDATA );
-#endif
-    picture->throwaway_frame[0] = tmp->base;
-    picture->throwaway_frame[1] = tmp->base + frame_size * 5 / 4;
-    picture->throwaway_frame[2] = tmp->base + frame_size;
+    frame[0] = output->allocate_image_buffer (picture->coded_picture_width, 
+					      picture->coded_picture_height, 
+					      0x32315659);
 
-#ifdef __OMS__
-    tmp = output->allocate_image_buffer (picture->coded_picture_height, 
-					  picture->coded_picture_width, 
-					  0x32315659);
-#else
-    tmp = output->allocate_image_buffer2 (picture->coded_picture_width, 
-					  picture->coded_picture_height, 
-					  0x32315659 USERDATA );
-#endif
-    picture->backward_reference_frame[0] = tmp->base;
-    picture->backward_reference_frame[1] = tmp->base + frame_size * 5 / 4;
-    picture->backward_reference_frame[2] = tmp->base + frame_size;
-   
-#ifdef __OMS__ 
-    tmp = output->allocate_image_buffer (picture->coded_picture_height, 
-					  picture->coded_picture_width, 
-					  0x32315659);
-#else
-    tmp = output->allocate_image_buffer2 (picture->coded_picture_width, 
-					  picture->coded_picture_height, 
-					  0x32315659 USERDATA );
-#endif
-    picture->forward_reference_frame[0] = tmp->base;
-    picture->forward_reference_frame[1] = tmp->base + frame_size * 5 / 4;
-    picture->forward_reference_frame[2] = tmp->base + frame_size;
+    picture->throwaway_frame[0] = frame[0]->base;
+    picture->throwaway_frame[1] = frame[0]->base + frame_size * 5 / 4;
+    picture->throwaway_frame[2] = frame[0]->base + frame_size;
+
+    frame[1] = output->allocate_image_buffer (picture->coded_picture_width, 
+					      picture->coded_picture_height, 
+					      0x32315659);
+    picture->backward_reference_frame[0] = frame[1]->base;
+    picture->backward_reference_frame[1] = frame[1]->base + frame_size * 5 / 4;
+    picture->backward_reference_frame[2] = frame[1]->base + frame_size;
+    
+    frame[2] = output->allocate_image_buffer (picture->coded_picture_width, 
+					      picture->coded_picture_height, 
+					      0x32315659);
+    picture->forward_reference_frame[0] = frame[2]->base;
+    picture->forward_reference_frame[1] = frame[2]->base + frame_size * 5 / 4;
+    picture->forward_reference_frame[2] = frame[2]->base + frame_size;
+#undef picture
+#undef output
+#undef frame
 }
 
 
-static void decode_reorder_frames (void)
+static void decode_reorder_frames (mpeg2dec_t * mpeg2dec)
 {
+#define picture (*(mpeg2dec->picture))
     if (picture.picture_coding_type != B_TYPE) {
 
 	//reuse the soon to be outdated forward reference frame
@@ -177,11 +191,20 @@ static void decode_reorder_frames (void)
 	picture.current_frame[2] = picture.throwaway_frame[2];
 
     }
+#undef picture
 }
 
 
-static int parse_chunk (INITTYPE * output, int code, uint8_t * buffer)
+static int parse_chunk (mpeg2dec_t * mpeg2dec, int code, uint8_t * buffer)
 {
+#define picture ((*mpeg2dec->picture))
+#define output  (mpeg2dec->output)
+#define is_sequence_needed (mpeg2dec->is_sequence_needed)
+#define in_slice (mpeg2dec->in_slice)
+#define drop_frame (mpeg2dec->drop_frame)
+#define drop_flag (mpeg2dec->drop_flag)
+#define is_display_initialized (mpeg2dec->is_display_initialized)
+
     int is_frame_done;
 
     if (is_sequence_needed && (code != 0xb3))	/* b3 = sequence_header_code */
@@ -246,27 +269,23 @@ static int parse_chunk (INITTYPE * output, int code, uint8_t * buffer)
 	    in_slice = 1;
 
 	    if (!is_display_initialized) {
-		ATTR_TYPE attr;
 
-		attr.width = picture.coded_picture_width;
-		attr.height = picture.coded_picture_height;
-		attr.fullscreen = 0;
-		attr.title = NULL;
-#ifndef __OMS__
-		attr.format = 0x32315659;
-#endif
+		mpeg2dec->attr.width = picture.coded_picture_width;
+		mpeg2dec->attr.height = picture.coded_picture_height;
+		mpeg2dec->attr.fullscreen = 0;
+		mpeg2dec->attr.title = NULL;
 
-		if (output->setup (&attr USERDATA)) {
+		if (output->setup (&(mpeg2dec->attr))) {
 		    printf ("display init failed\n");
 		    exit (1);
 		}
-
-		decode_allocate_image_buffers (output, &picture);
-		decode_reorder_frames ();
+		decode_allocate_image_buffers (mpeg2dec);
+		decode_reorder_frames (mpeg2dec);
 		is_display_initialized = 1;
-	    } else if (!picture.second_field)
-		decode_reorder_frames ();
+	    } else if (!picture.second_field) 
+		decode_reorder_frames (mpeg2dec);
 	}
+
 
 	drop_frame |= drop_flag && (picture.picture_coding_type == B_TYPE);
 	
@@ -275,9 +294,9 @@ static int parse_chunk (INITTYPE * output, int code, uint8_t * buffer)
 
 	    if ((HACK_MODE < 2) && (!picture.mpeg1)) {
 		uint8_t * foo[3];
-		uint8_t ** bar;
 		int offset;
-
+                uint8_t ** bar;
+                
 		if (picture.picture_coding_type == B_TYPE)
 		    bar = picture.throwaway_frame;
 		else
@@ -302,14 +321,21 @@ static int parse_chunk (INITTYPE * output, int code, uint8_t * buffer)
     }
 
     return is_frame_done;
+#undef picture
+#undef output
+#undef is_sequence_needed
+#undef in_slice
+#undef drop_frame
+#undef drop_flag
+#undef is_display_initialized
 }
 
-int mpeg2_decode_data (INITTYPE *output, uint8_t *current, uint8_t *end)
+int mpeg2_decode_data_ng (mpeg2dec_t* mpeg2dec, uint8_t *current, uint8_t *end)
 {
-    static uint8_t code = 0xff;
-    //static uint8_t chunk_buffer[65536];
-    static uint8_t *chunk_ptr = chunk_buffer;
-    //static uint32_t shift = 0;
+#define shift (mpeg2dec->shift)
+#define chunk_buffer (mpeg2dec->chunk_buffer)
+#define chunk_ptr (mpeg2dec->chunk_ptr)
+#define code (mpeg2dec->code)
 
     uint8_t byte;
     int ret = 0;
@@ -328,39 +354,49 @@ int mpeg2_decode_data (INITTYPE *output, uint8_t *current, uint8_t *end)
 
 	/* found start_code following chunk */
 
-	ret += parse_chunk (output, code, chunk_buffer);
+	ret += parse_chunk (mpeg2dec, code, chunk_buffer);
 
 	/* done with header or slice, prepare for next one */
-
 	code = byte;
 	chunk_ptr = chunk_buffer;
 	shift = 0xffffff00;
     }
-
     return ret;
+#undef shift
+#undef chunk_buffer
+#undef chunk_ptr
+#undef code
 }
 
-void mpeg2_close (INITTYPE * output)
+void mpeg2_close_ng (mpeg2dec_t * mpeg2dec)
 {
+#define output (mpeg2dec->output)
+#define is_display_initialized (mpeg2dec->is_display_initialized)
+#define picture (mpeg2dec->picture)
+#define frame (mpeg2dec->frame)
+
     static uint8_t finalizer[] = {0,0,1,0};
 
-    mpeg2_decode_data (output, finalizer, finalizer+4);
+    mpeg2_decode_data_ng (mpeg2dec, finalizer, finalizer+4);
 
     if (is_display_initialized)
-	output->draw_frame (picture.backward_reference_frame);
-#if 0 // Totaly broken, wrong type and not implemented in all outputs
-    output->free_image_buffer (picture.backward_reference_frame);
-    output->free_image_buffer (picture.forward_reference_frame);
-    output->free_image_buffer (picture.throwaway_frame);
-#endif
+	output->draw_frame (picture->backward_reference_frame);
+
+    output->free_image_buffer (frame[0]);
+    output->free_image_buffer (frame[1]);
+    output->free_image_buffer (frame[2]);
+#undef output
+#undef is_display_initialized
+#undef picture
+#undef frame
 }
 
-void mpeg2_drop (int flag)
+void mpeg2_drop_ng (mpeg2dec_t * mpeg2dec, int flag)
 {
-    drop_flag = flag;
+    mpeg2dec->drop_flag = flag;
 }
 
-void mpeg2_output_init (int flag)
+void mpeg2_output_init_ng (mpeg2dec_t * mpeg2dec,int flag)
 {
-    is_display_initialized = flag;
+    mpeg2dec->is_display_initialized = flag;
 }
--- mpeg2dec-livid/libmpeg2/mpeg2.h
+++ mpeg2dec-livid/libmpeg2/mpeg2.h
@@ -19,16 +19,104 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-// FIXME try to get rid of that...
+//
+// We should remove the typdefs in oms for vo_image_buffer_t 
+// plugin_output_video_attr_t and let OMS include
+// them from the video.
+// INCLUDES ="${INCLUDES} $video_lib_includes" and let video lib
+// define this :-)
+
 #ifdef __OMS__
-#include <oms/plugin/output_video.h>
-#ifndef vo_functions_t
-#define vo_functions_t plugin_output_video_t
-#endif
+#include "../../output/video/output_video.h"
+#else
+#include "../libvo/output_video.h"
 #endif
 
+//
+// Structure for the mpeg2dec decoder.
+// 
+
+//
+// Backward compatible old interface [START]
+//
+
 void mpeg2_init (void);
-int mpeg2_decode_data (vo_functions_t *, uint8_t * data_start, uint8_t * data_end);
-void mpeg2_close (vo_functions_t *);
+int mpeg2_decode_data (vo_output_video_t *, 
+		       uint8_t * data_start, uint8_t * data_end);
+void mpeg2_close (vo_output_video_t *);
 void mpeg2_drop (int flag);
 void mpeg2_output_init (int flag);
+
+
+//
+// Backward compatible old interface [END]
+//
+
+
+//
+// Proposed new interface [START]
+//
+
+
+// forward declaration (hidden in mpeg2_internal.h)
+#ifndef __MPEG2_INTERNAL
+typedef void* picture_t;
+#endif
+
+
+typedef struct mpeg2dec_s {
+    vo_output_video_t* output;
+    vo_output_video_attr_t attr;
+    void* user_data;
+
+    // here we store the allocated images, which we get from
+    // the output interface
+    img_buf_t * frame[3];
+ 
+    // this is where we keep the state of the decoder
+    picture_t * picture;
+    
+    uint32_t shift;
+    int is_display_initialized;
+    int is_sequence_needed;
+    int drop_flag;
+    int drop_frame;
+    int in_slice;
+
+    // the maximum chunk size is determined by vbv_buffer_size 
+    // which is 224K for
+    // MP@ML streams. 
+    // (we make no pretenses ofdecoding anything more than that)
+    // allocated in init gcc has problems allocating
+    // such big structures
+    uint8_t* chunk_buffer;
+    // pointer to current position in chunk_buffer
+    uint8_t* chunk_ptr;
+    // last start code ?
+    uint8_t code;
+
+} mpeg2dec_t ;
+
+
+
+
+
+// initialize mpegdec with a opaque user pointer
+// if not needed in the output use NULL here
+void mpeg2_init_ng (mpeg2dec_t* mpeg2dec,
+		    vo_output_video_t* output,void* user_data);
+
+// destroy everything which was allocated, shutdown the output
+void mpeg2_close_ng (mpeg2dec_t* mpeg2dec);
+
+
+int mpeg2_decode_data_ng (mpeg2dec_t* mpeg2dec,
+			  uint8_t * data_start, uint8_t * data_end);
+
+void mpeg2_drop_ng (mpeg2dec_t* mpeg2dec, int flag);
+void mpeg2_output_init_ng (mpeg2dec_t* mpeg2dec, int flag);
+
+
+//
+// Proposed new interface [END]
+//
