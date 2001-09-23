a7c3af8ff37db99b4212a080d2327c9877aa0d1f
walken <walken@69194b2d-ca13-0410-9ec0-9203f42c7898>
2001-09-23 23:22:45 +0000

    I dont think aaron is interested into mpeg2dec anymore.

--- mpeg2dec/libvo/yuv2rgb_mmx.c
+++ mpeg2dec/libvo/yuv2rgb_mmx.c
@@ -6,6 +6,7 @@
  * Author: Olie Lho <ollie@sis.com.tw>
  *
  * This file is part of mpeg2dec, a free MPEG-2 video stream decoder.
+ * See http://libmpeg2.sourceforge.net/ for updates.
  *
  * mpeg2dec is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
