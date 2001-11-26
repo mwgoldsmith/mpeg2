fdfbbf25a7b6bb18fdfe7ea5d0e307f7fe3b0dd1
walken <walken@69194b2d-ca13-0410-9ec0-9203f42c7898>
2001-11-26 10:46:00 +0000

    added support for vc++ builds

---
+++ mpeg2dec/vc++/config.h
@@ -0,0 +1,69 @@
+/* vc++/config.h - manually adapted from include/config.h.in */
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define as __inline if that's what the C compiler calls it.  */
+#define inline __inline
+
+/* Define as the return type of signal handlers (int or void).  */
+/* #undef RETSIGTYPE */
+
+/* Define to `unsigned' if <sys/types.h> doesn't define.  */
+/* #undef size_t */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if your processor stores words with the most significant
+   byte first (like Motorola and SPARC, unlike Intel and VAX).  */
+/* #undef WORDS_BIGENDIAN */
+
+/* Define if the X Window System is missing or not being used.  */
+/* #undef X_DISPLAY_MISSING */
+
+/* Define if you have the memalign function.  */
+/* #undef HAVE_MEMALIGN */
+
+/* Define if you have the <string.h> header file.  */
+#define HAVE_STRING_H 1
+
+/* Define if you have the <sys/time.h> header file.  */
+/* #undef HAVE_SYS_TIME_H */
+
+/* Name of package */
+#define PACKAGE "mpeg2dec"
+
+/* Version number of package */
+#define VERSION "0.2.1-cvs"
+
+/* x86 architecture */
+/* #undef ARCH_X86 */
+
+/* ppc architecture */
+/* #undef ARCH_PPC */
+
+/* libmpeg2 mediaLib support */
+/* #undef LIBMPEG2_MLIB */
+
+/* libvo X11 support */
+/* #undef LIBVO_X11 */
+
+/* libvo Xv support */
+/* #undef LIBVO_XV */
+
+/* libvo mediaLib support */
+/* #undef LIBVO_MLIB */
+
+/* libvo SDL support */
+/* #undef LIBVO_SDL */
+
+/* libvo MGA support */
+/* #undef LIBVO_MGA */
+
+/* mpeg2dec profiling */
+/* #undef MPEG2DEC_GPROF */
+
+/* maximum supported data alignment */
+/* #undef ATTRIBUTE_ALIGNED_MAX */
+
