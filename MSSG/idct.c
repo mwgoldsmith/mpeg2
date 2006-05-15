12f412d133efe158191d68dd28951044cfa0179c
sam <sam@69194b2d-ca13-0410-9ec0-9203f42c7898>
2006-05-15 03:04:22 +0000

    * Reorganised new SVN repository.

--- MSSG/idct.c
+++
@@ -1,9 +0,0 @@
-/* two dimensional inverse discrete cosine transform */
-void Fast_IDCT(block, comp)
-  short *block;
-  int comp;
-{
-    idct_c (block);
-    //idct_mmx (block, comp);
-    //idct_altivec (block);
-}
