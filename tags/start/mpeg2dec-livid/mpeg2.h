7c7cb452b396c767e16cc737387f8bae523cf74b
sam <sam@69194b2d-ca13-0410-9ec0-9203f42c7898>
2006-05-15 03:04:22 +0000

    * Reorganised new SVN repository.

--- tags/start/mpeg2dec-livid/mpeg2.h
+++
@@ -1,8 +0,0 @@
-
-//typedef to appropriate type for your architecture
-typedef unsigned char uint_8;
-typedef unsigned short uint_16;
-typedef unsigned int uint_32;
-typedef signed int sint_32;
-typedef signed short sint_16;
-typedef signed char sint_8;
--- tags/start/mpeg2dec-livid/slice.h
+++
@@ -1,7 +0,0 @@
-
-void slice_init(void);
-
-
-void slice_new(picture_t *picture);
-void slice_reset_dc_pred(slice_t* slice);
-void slice_reset_pmv(slice_t* slice);
