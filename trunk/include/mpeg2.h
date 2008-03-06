c642b675be0960230c7e720ced138f4f52da70e4
massiot <massiot@69194b2d-ca13-0410-9ec0-9203f42c7898>
2008-03-06 15:51:50 +0000

    * ALL: support for ARM-specific optimizations, code by Koji Agawa.

--- trunk/include/mpeg2.h
+++ trunk/include/mpeg2.h
@@ -162,6 +162,7 @@ void mpeg2_custom_fbuf (mpeg2dec_t * mpeg2dec, int custom_fbuf);
 #define MPEG2_ACCEL_ALPHA_MVI 2
 #define MPEG2_ACCEL_SPARC_VIS 1
 #define MPEG2_ACCEL_SPARC_VIS2 2
+#define MPEG2_ACCEL_ARM 1
 #define MPEG2_ACCEL_DETECT 0x80000000
 
 uint32_t mpeg2_accel (uint32_t accel);
